2c3ca72fb2253a60eb7e057d237f9f56f3b7bb26
shenerchelebi <shenerchelebi@gmail.com>
2025-07-02 16:45:37 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to the Association of Turks in Bulgaria</h1>
-      <p>This is a placeholder landing page.</p>
-    </div>
-  );
-}
