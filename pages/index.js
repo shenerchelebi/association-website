53a29749225ebc0b10bb4047f2c161b189017094
shenerchelebi <shenerchelebi@gmail.com>
2025-07-02 15:00:51 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: '4rem', fontFamily: 'sans-serif' }}>
+      <h1>Association of Turks in Bulgaria</h1>
+      <p>Welcome to our official website. More coming soon.</p>
+    </div>
+  );
+}
