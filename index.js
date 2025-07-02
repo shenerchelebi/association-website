0a3657aa058fac6154fd1fc4668a848f53bb1f74
shenerchelebi <shenerchelebi@gmail.com>
2025-07-02 14:06:43 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'Arial' }}>
+      <h1>Association of Turks in Bulgaria</h1>
+      <p>Welcome to our official site. Preserving culture. Empowering community.</p>
+    </div>
+  );
+}
