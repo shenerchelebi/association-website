3ce244838c840d9a7ae77c8a2f1fd0f9131ebfd6
shenerchelebi <shenerchelebi@gmail.com>
2025-07-03 03:40:25 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/globals.css';
+import styles from '../styles/';
 
 export default function Home() {
   return (
