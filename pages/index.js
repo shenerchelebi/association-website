8587a35b0b45223e79a249d10943275a7cb76cad
shenerchelebi <shenerchelebi@gmail.com>
2025-07-03 02:12:58 +0100

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-
-export default function HomePage() {
-  return (
-    <div style={{ padding: '4rem', fontFamily: 'Arial', textAlign: 'center' }}>
-      <h1>Welcome to the Association of Turks in Bulgaria</h1>
-      <p>This is our official homepage. More updates coming soon.</p>
-    </div>
-  );
-}
